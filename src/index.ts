dfa9e33d730a1841093dbae1d76c67fc70d87121
atvsipi <atvsipi@gmail.com>
2024-10-11 11:06:13 +0900

    fix food team

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import {Entity} from './entity/entity';
 import {Color} from './definitions/color';
 import {Vector} from './physics/vector';
 import {Nearest} from './entity/controller';
+import {Team} from './definitions/team';
 
 Listen(3003, () => {
     Logger.success('Web server is listen on 3003!');
@@ -80,6 +81,8 @@ setInterval(() => {
         const entity = new Entity();
 
         entity.init(EntityClass.Food);
+        entity.pos = new Vector(RoomConfig.width * Math.random(), RoomConfig.height * Math.random());
+        entity.team = Team.Room;
         entity.pos = new Vector(RoomConfig.width * Math.random(), RoomConfig.height * Math.random());
 
         room.insert(entity);
