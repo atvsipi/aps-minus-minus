6eb4bc42992e8ebd1b6e53e653918705001f6c83
atvsipi <atvsipi@gmail.com>
2024-10-18 17:34:17 +0900

    good

--- public/lib/score.js
+++ public/lib/score.js
@@ -9,45 +9,29 @@ class Score {
 
     scoreProgress = 0;
 
-    lastLevel = 0;
-    animate = false;
-
     update() {
-        if (!this.animate) {
-            if (this.lastLevel < this.level) return (this.animate = true);
-
-            if (this.levelScore !== this.lastLevelScore) {
-                this.oldLevelScore = this.lastLevelScore;
-                this.lastLevelScore = this.levelScore;
-            }
-
-            let scoreProgress = (this.score - this.oldLevelScore) / (this.lastLevelScore - this.oldLevelScore);
-
-            if (scoreProgress === Infinity) scoreProgress = 1;
-            if (scoreProgress === -Infinity) scoreProgress = 0;
-            if (isNaN(scoreProgress)) scoreProgress = 0;
-
-            if (scoreProgress !== this.scoreProgress) {
-                const diff = Math.abs(scoreProgress - this.scoreProgress);
-
-                if (diff < 0.001) {
-                    this.scoreProgress = scoreProgress;
-                } else {
-                    this.scoreProgress += (scoreProgress - this.scoreProgress) / 30;
-                }
-            }
-
-            if (isNaN(this.scoreProgress)) this.scoreProgress = 0;
-        } else {
-            if (this.scoreProgress < -0.2) {
-                this.animate = false;
-                this.lastLevel = this.level;
-
-                return;
-            }
-
-            this.scoreProgress -= 0.03;
+        if (this.levelScore !== this.lastLevelScore) {
+            this.oldLevelScore = this.lastLevelScore;
+            this.lastLevelScore = this.levelScore;
         }
+
+        let scoreProgress = (this.score - this.oldLevelScore) / (this.lastLevelScore - this.oldLevelScore);
+
+        if (scoreProgress === Infinity) scoreProgress = 1;
+        if (scoreProgress === -Infinity) scoreProgress = 0;
+        if (isNaN(scoreProgress)) scoreProgress = 0;
+
+        if (scoreProgress !== this.scoreProgress) {
+            const diff = Math.abs(scoreProgress - this.scoreProgress);
+
+            if (diff < 0.001) {
+                this.scoreProgress = scoreProgress;
+            } else {
+                this.scoreProgress += (scoreProgress - this.scoreProgress) / 30;
+            }
+        }
+
+        if (isNaN(this.scoreProgress)) this.scoreProgress = 0;
     }
 }
 
